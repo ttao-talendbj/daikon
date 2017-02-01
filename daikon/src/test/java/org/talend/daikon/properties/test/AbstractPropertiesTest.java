66182522a78f9dbcd3609a2f5687731dc7eaaee9
RyanSkraba <rskraba@talend.com>
2017-02-01 11:31:40 +0100

    Add helper method to assertComponentIsRegistered. (#140)
    * Add helper method to assertComponentIsRegistered.
    
    * refactor: code formatting with eclipse plugin.

--- daikon/src/test/java/org/talend/daikon/properties/test/AbstractPropertiesTest.java
+++ daikon/src/test/java/org/talend/daikon/properties/test/AbstractPropertiesTest.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2017 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -12,7 +12,11 @@
 // ============================================================================
 package org.talend.daikon.properties.test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -29,8 +33,8 @@ public abstract class AbstractPropertiesTest {
     abstract public DefinitionRegistryService getDefinitionRegistry();
 
     /**
-     * checks that all properties created from all the definition in the registry have a proper i18n displayName and title.
-     * As well as checking for each Property and nested Properties.
+     * checks that all properties created from all the definition in the registry have a proper i18n displayName and
+     * title. As well as checking for each Property and nested Properties.
      */
     @Test
     public void testAlli18n() {
@@ -45,10 +49,28 @@ public abstract class AbstractPropertiesTest {
         PropertiesTestUtils.assertAllImagesAreSetup(getDefinitionRegistry(), errorCollector);
     }
 
+    /**
+     * @deprecated Use {@link #assertComponentIsRegistered(Class, String, Class)} with the expected class types.
+     */
     public void assertComponentIsRegistered(String definitionName) {
         Definition definition = getDefinitionRegistry().getDefinitionsMapByType(Definition.class).get(definitionName);
         assertNotNull("Could not find the definition [" + definitionName + "], please check the registered definitions",
                 definition);
+    }
+
+    /**
+     * Check that the set of Definitions that correspond to requestClass contain the value with the given definitionName
+     * and definitionClass.
+     * 
+     * @param requestClass The set of definitions to request by superclass.
+     * @param definitionName The name of the definition to request.
+     * @param definitionClass The expected type of class returned.
+     */
+    public void assertComponentIsRegistered(Class<? extends Definition> requestClass, String definitionName,
+            Class<? extends Definition> definitionClass) {
+        assertThat("Could not find the definition [" + definitionName + ", " + definitionClass + "]",
+                getDefinitionRegistry().getDefinitionsMapByType(requestClass),
+                hasEntry(is(definitionName), instanceOf(definitionClass)));
     }
 
 }
