980ce35ebb4999f0ce5a30767ab2847a3bc45a9a
Francois Huaulme <fhuaulme@talend.com>
2017-03-16 19:54:12 +0100

    [Fix] Don't enable ContentService if no resource loader found. (#206)

--- daikon-content-service/content-service-common/src/main/java/org/talend/daikon/content/ContentServiceImpl.java
+++ daikon-content-service/content-service-common/src/main/java/org/talend/daikon/content/ContentServiceImpl.java
@@ -1,15 +1,13 @@
 package org.talend.daikon.content;
 
-import static org.talend.daikon.content.ContentErrorCode.UNABLE_TO_CLEAR_CONTENT;
-import static org.talend.daikon.content.ContentErrorCode.UNABLE_TO_DELETE_CONTENT;
-import static org.talend.daikon.content.ContentErrorCode.UNABLE_TO_GET_CONTENT;
-import static org.talend.daikon.content.ContentErrorCode.UNABLE_TO_PUT_CONTENT;
+import static org.talend.daikon.content.ContentErrorCode.*;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.talend.daikon.annotation.ServiceImplementation;
@@ -17,6 +15,7 @@ import org.talend.daikon.exception.TalendRuntimeException;
 import org.talend.services.ContentService;
 
 @ServiceImplementation
+@ConditionalOnBean(ResourceResolver.class)
 public class ContentServiceImpl implements ContentService {
 
     @Autowired
