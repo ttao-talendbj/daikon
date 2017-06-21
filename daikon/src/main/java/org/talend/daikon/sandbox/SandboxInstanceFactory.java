d4b469b6add74e3a7d51de1e5efc63ccde858e2b
RyanSkraba <rskraba@talend.com>
2017-06-21 11:20:14 +0200

    fix(TCOMP-422): Add finer-grained control to sandbox caching. (#249)
    * fix(TCOMP-422): Add finer-grained control to sandbox caching.
    
    * doc: change date to 2017.
    
    * chore: remove unused imports

--- daikon/src/main/java/org/talend/daikon/sandbox/SandboxInstanceFactory.java
+++ daikon/src/main/java/org/talend/daikon/sandbox/SandboxInstanceFactory.java
@@ -29,8 +29,8 @@ import org.talend.java.util.ClosableLRUMap;
 public class SandboxInstanceFactory {
 
     /**
-     * TODO: Add context variable to allow the user to configure the maximum size of the cache.
-     * Maybe using a CacheBuilder.
+     * TODO: Add context variable to allow the user to configure the maximum size of the cache. Maybe using a
+     * CacheBuilder.
      */
     static Map<RuntimeInfo, ClassLoader> classLoaderCache = Collections
             .synchronizedMap(new ClosableLRUMap<RuntimeInfo, ClassLoader>(3, 10));
@@ -45,16 +45,13 @@ public class SandboxInstanceFactory {
 
     /**
      * This will create a new class instance base on an URLClassLoader using the <code>classPathUrls</code> and the
-     * <code>parentClassLoader</code>.
-     * This instance will be embed in a {@link SandboxedInstance} so that it provides a SystemProperty isolation.<brW
-     * All the isolation constraints are to be found in the {@link SandboxedInstance} javadoc, please make sure you read it
-     * carefully.
+     * <code>parentClassLoader</code>. This instance will be embed in a {@link SandboxedInstance} so that it provides a
+     * SystemProperty isolation.<brW All the isolation constraints are to be found in the {@link SandboxedInstance}
+     * javadoc, please make sure you read it carefully.
      * 
-     * @param classToInstanciate name of the class to instantiate
-     * @param classPathUrls set of URLs to be used for creating a new {@link ClassLoader}.
      * @param parentClassLoader used a parent ClassLoader for the newly created classloader, may be null.
-     * @param useCurrentJvmProperties if true, a copy of the current jvm system properties will be used, if false then a default
-     *            jvm set of properties (see {@link StandardPropertiesStrategyFactory} will be used
+     * @param useCurrentJvmProperties if true, a copy of the current jvm system properties will be used, if false then a
+     * default jvm set of properties (see {@link StandardPropertiesStrategyFactory} will be used
      * 
      * @return a SandboxedInstance object ready for System Properties isolation
      */
@@ -63,27 +60,44 @@ public class SandboxInstanceFactory {
         if (runtimeInfo.getRuntimeClassName() == null) {
             throw new IllegalArgumentException("classToInstantiate should not be null");
         }
-        synchronized (classLoaderCache) {
-            if (classLoaderCache.containsKey(runtimeInfo) && classLoaderCache.get(runtimeInfo) != null) {
-                return new SandboxedInstance(runtimeInfo.getRuntimeClassName(), useCurrentJvmProperties,
-                        classLoaderCache.get(runtimeInfo));
-            } else {
-                // the following classloader is closeable so there is a possible resource leak.
-                // if the returned SandboxInstance is properly closed this classLoader shall be closed too.
-                ClassLoader sandboxClassLoader = new URLClassLoader(
-                        runtimeInfo.getMavenUrlDependencies().toArray(new URL[runtimeInfo.getMavenUrlDependencies().size()]),
-                        parentClassLoader) {
 
-                    @Override
-                    public void close() throws IOException {
-                        super.close();
-                        ClassLoaderIsolatedSystemProperties.getInstance().stopIsolateClassLoader(this);
-                    }
-                };
-                classLoaderCache.put(runtimeInfo, sandboxClassLoader);
-                return new SandboxedInstance(runtimeInfo.getRuntimeClassName(), useCurrentJvmProperties, sandboxClassLoader);
+        // Determine whether the ClassLoader for the runtimeInfo can be safely cached for performance.
+        ClassLoader sandboxClassLoader;
+        boolean reusableClassLoader = true;
+        if (runtimeInfo instanceof SandboxControl)
+            reusableClassLoader = ((SandboxControl) runtimeInfo).isClassLoaderReusable();
+
+        if (reusableClassLoader) {
+            // When the ClassLoader is reusable, use it from the cache.
+            synchronized (classLoaderCache) {
+                if (classLoaderCache.containsKey(runtimeInfo) && classLoaderCache.get(runtimeInfo) != null) {
+                    sandboxClassLoader = classLoaderCache.get(runtimeInfo);
+                } else {
+                    // the following classloader is closeable so there is a possible resource leak.
+                    // if the returned SandboxInstance is properly closed this classLoader shall be closed too.
+                    sandboxClassLoader = createClassLoader(runtimeInfo, parentClassLoader);
+                    classLoaderCache.put(runtimeInfo, sandboxClassLoader);
+                }
             }
+        } else {
+            // When the ClassLoader is not reusable, never used a cached instance or save it for reuse.
+            sandboxClassLoader = createClassLoader(runtimeInfo, parentClassLoader);
         }
+
+        return new SandboxedInstance(runtimeInfo.getRuntimeClassName(), useCurrentJvmProperties, sandboxClassLoader);
+    }
+
+    private static URLClassLoader createClassLoader(RuntimeInfo runtimeInfo, ClassLoader parentClassLoader) {
+        return new URLClassLoader(
+                runtimeInfo.getMavenUrlDependencies().toArray(new URL[runtimeInfo.getMavenUrlDependencies().size()]),
+                parentClassLoader) {
+
+            @Override
+            public void close() throws IOException {
+                super.close();
+                ClassLoaderIsolatedSystemProperties.getInstance().stopIsolateClassLoader(this);
+            }
+        };
     }
 
     public static void clearCache() {
