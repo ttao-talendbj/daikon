ca8fa5f3a735919d11211d592c3b66b96edf3054
cnguyen <cnguyen@talend.com>
2017-10-24 14:50:08 +0200

    feat (TDP-4342): fix codacy errors

--- daikon-tql/daikon-tql-core/src/main/resources/tql/index.js
+++ daikon-tql/daikon-tql-core/src/main/resources/tql/index.js
@@ -3,44 +3,43 @@ var TqlParser = require("./TqlParser").TqlParser;
 var TqlListener = require("./TqlParserListener").TqlParserListener;
 
 var antlr4 = require("antlr4/index");
-var parse = function (
-                        tql,
-                        onExactFilter,
-                        onContainsFilter,
-                        onCompliesFilter,
-                        onBetweenFilter,
-                        onEmptyFilter,
-                        onValidFilter,
-                        onInvalidFilter) {
+var parse = function(tql,
+                     onExactFilter,
+                     onContainsFilter,
+                     onCompliesFilter,
+                     onBetweenFilter,
+                     onEmptyFilter,
+                     onValidFilter,
+                     onInvalidFilter) {
 
-                 var chars = new antlr4.InputStream(tql);
-                 var lexer = new TqlLexer(chars);
-                 var tokens  = new antlr4.CommonTokenStream(lexer);
-                 var parser = new TqlParser(tokens);
-                 parser.buildParseTrees = true;
-                 var tree = parser.expression();
+                         var chars = new antlr4.InputStream(tql);
+                         var lexer = new TqlLexer(chars);
+                         var tokens  = new antlr4.CommonTokenStream(lexer);
+                         var parser = new TqlParser(tokens);
+                         parser.buildParseTrees = true;
+                         var tree = parser.expression();
 
-                 //Define listeners
-                 var noop = () => {};
-                 var listener = new TqlListener();
-                 listener.enterLiteralComparison = (ctx) => {
-                     switch (ctx.children[1].getText()) {
-                     case "=":
-                         onExactFilter ? onExactFilter(ctx) : noop();
-                         break;
-                     default:
-                         return noop();
-                     }
-                 };
-                 listener.enterFieldIsEmpty = onEmptyFilter || noop;
-                 listener.enterFieldIsValid = onValidFilter || noop;
-                 listener.enterFieldIsInvalid = onInvalidFilter || noop;
-                 listener.enterFieldContains = onContainsFilter || noop;
-                 listener.enterFieldCompliesPattern = onCompliesFilter || noop;
-                 listener.enterFieldBetween = onBetweenFilter || noop;
+                         //Define listeners
+                         var noop = function() {};
+                         var listener = new TqlListener();
+                         listener.enterLiteralComparison = function(ctx) {
+                             switch (ctx.children[1].getText()) {
+                             case "=":
+                                 onExactFilter ? onExactFilter(ctx) : noop();
+                                 break;
+                             default:
+                                 return noop();
+                             }
+                         };
+                         listener.enterFieldIsEmpty = onEmptyFilter || noop;
+                         listener.enterFieldIsValid = onValidFilter || noop;
+                         listener.enterFieldIsInvalid = onInvalidFilter || noop;
+                         listener.enterFieldContains = onContainsFilter || noop;
+                         listener.enterFieldCompliesPattern = onCompliesFilter || noop;
+                         listener.enterFieldBetween = onBetweenFilter || noop;
 
-                 //Bind listeners to tree
-                 antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
-             };
+                         //Bind listeners to tree
+                         antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
+                     };
 
 export { TqlLexer, TqlParser, TqlListener, parse };
