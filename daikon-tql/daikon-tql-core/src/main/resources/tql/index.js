bace4ad8b485148fa7a0af28417ceb8b439a42d2
cnguyen <cnguyen@talend.com>
2017-10-24 14:14:47 +0200

    feat (TDP-4342): add method to parse TQL

--- daikon-tql/daikon-tql-core/src/main/resources/tql/index.js
+++ daikon-tql/daikon-tql-core/src/main/resources/tql/index.js
@@ -2,4 +2,38 @@ var TqlLexer = require("./TqlLexer").TqlLexer;
 var TqlParser = require("./TqlParser").TqlParser;
 var TqlListener = require("./TqlParserListener").TqlParserListener;
 
-export { TqlLexer, TqlParser, TqlListener };
+var antlr4 = require('antlr4/index');
+var parse = (tql, onExactFilter, onContainsFilter, onCompliesFilter,
+             onBetweenFilter, onEmptyFilter, onValidFilter, onInvalidFilter) => {
+
+                 var chars = new antlr4.InputStream(tql);
+                 var lexer = new TqlLexer(chars);
+                 var tokens  = new antlr4.CommonTokenStream(lexer);
+                 var parser = new TqlParser(tokens);
+                 parser.buildParseTrees = true;
+                 var tree = parser.expression();
+
+                 //Define listeners
+                 var noop = () => {};
+                 var listener = new TqlParserListener();
+                 listener.enterLiteralComparison = (ctx) => {
+                     switch (ctx.children[1].getText()) {
+                     case '=':
+                         onExactFilter ? onExactFilter(ctx) : noop();
+                         break;
+                     default:
+                         return noop();
+                     }
+                 };
+                 listener.enterFieldIsEmpty = onEmptyFilter || noop;
+                 listener.enterFieldIsValid = onValidFilter || noop;
+                 listener.enterFieldIsInvalid = onInvalidFilter || noop;
+                 listener.enterFieldContains = onContainsFilter || noop;
+                 listener.enterFieldCompliesPattern = onCompliesFilter || noop;
+                 listener.enterFieldBetween = onBetweenFilter || noop;
+
+                 //Bind listeners to tree
+                 antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
+             }
+
+export { TqlLexer, TqlParser, TqlListener, parse };
