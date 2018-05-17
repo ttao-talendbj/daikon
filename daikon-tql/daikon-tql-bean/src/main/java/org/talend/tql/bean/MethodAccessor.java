79900d9b742ea53e288c052ed3d2283c28bb34d6
Francois Huaulme <fhuaulme@talend.com>
2018-05-17 15:52:16 +0200

    fix(TDKN-198): Add support for list match in TQL -> BeanPredicate con… (#366)
    * fix(TDKN-198): Add support for list match in TQL -> BeanPredicate conversion
    * Fix method invocation path find to handle Iterable<T> return types.
    * feat(TDKN-199): Add support for @JsonProperty to find value getter.
    
    (cherry picked from commit 4295f70)
    (cherry picked from commit 21d5594)

---
+++ daikon-tql/daikon-tql-bean/src/main/java/org/talend/tql/bean/MethodAccessor.java
@@ -0,0 +1,26 @@
+package org.talend.tql.bean;
+
+import java.util.Set;
+
+/**
+ * A utility to access method return objects and encapsulate different behaviors to apply in case of iterable values or
+ * unary values.
+ *
+ * @see MethodAccessorFactory
+ */
+interface MethodAccessor {
+
+    /**
+     * Apply the method on <b>all</b> input values and return all the values returned by method invocations on <code>o</code>.
+     *
+     * @param o All the values to apply.
+     * @return A new {@link Set} that contains values of method invocations on <code>o</code>.
+     */
+    Set<Object> getValues(Set<Object> o);
+
+    /**
+     * @return The class of the element returned by method. For Iterable based accessors, this should return the Iterable
+     * item type instead of the Iterable itself.
+     */
+    Class getReturnType();
+}
