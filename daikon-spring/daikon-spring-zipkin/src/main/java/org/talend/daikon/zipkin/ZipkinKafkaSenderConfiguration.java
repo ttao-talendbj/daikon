cb85e29f1a00d429d904b4b494cfaa8b1880237f
Aurélien Leboulanger <35097656+aleboulanger@users.noreply.github.com>
2018-06-22 15:24:20 +0000

    fix(TDP-4665): add talend prefix for properties names (#380)
    * use Spring BOM in order to manage spring dependencies versions
    
    * Zipkin is enabled by default so the configuration should be match if no spring.zipkin.enabled property

--- daikon-spring/daikon-spring-zipkin/src/main/java/org/talend/daikon/zipkin/ZipkinKafkaSenderConfiguration.java
+++ daikon-spring/daikon-spring-zipkin/src/main/java/org/talend/daikon/zipkin/ZipkinKafkaSenderConfiguration.java
@@ -1,47 +1,47 @@
 package org.talend.daikon.zipkin;
 
-import org.apache.kafka.common.serialization.ByteArraySerializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import zipkin2.reporter.Sender;
-import zipkin2.reporter.kafka11.KafkaSender;
-
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import zipkin2.reporter.Sender;
+import zipkin2.reporter.kafka11.KafkaSender;
+
 /**
  * Configures the zipkin kafka sender for another kafka broker than the functional broker.
- * spring.zipkin.kafka.bootstrapServers stores the broker dedicated to the infrastructure log events.
+ * @see ZipkinKafkaProperties
+ * @see KafkaSender
  */
 @Configuration
 @ConditionalOnClass(ByteArraySerializer.class)
-@ConditionalOnMissingBean(Sender.class)
-@ConditionalOnProperty(value = "spring.zipkin.enabled", havingValue = "true")
+@ConditionalOnProperty(value = "spring.zipkin.enabled", havingValue = "true", matchIfMissing = true)
+@EnableConfigurationProperties(ZipkinKafkaProperties.class)
 public class ZipkinKafkaSenderConfiguration {
 
     private static final Logger LOG = LoggerFactory.getLogger(ZipkinKafkaSenderConfiguration.class);
 
-    @Value("${spring.zipkin.kafka.topic:zipkin}")
-    private String topic;
-
-    @Value("${spring.zipkin.kafka.bootstrapServers}")
-    private String bootstrapServers;
-
     @Bean
-    public Sender kafkaSender() {
-        Map<String, Object> properties = new HashMap<>();
+    @ConditionalOnMissingBean
+    public Sender kafkaSender(ZipkinKafkaProperties kafkaProperties) {
+        LOG.info("building zipkin kafka sender with brokers [{}]", kafkaProperties.getBootstrapServers());
+
+        Map<String, Object> properties = new HashMap<>(2);
         properties.put("key.serializer", ByteArraySerializer.class.getName());
         properties.put("value.serializer", ByteArraySerializer.class.getName());
-        properties.put("bootstrap.servers", bootstrapServers);
 
-        LOG.info("building zipkin kafka sender with brokers " + bootstrapServers);
-
-        return KafkaSender.newBuilder().topic(this.topic).overrides(properties).build();
+        return KafkaSender.newBuilder() //
+                .topic(kafkaProperties.getTopic()) //
+                .bootstrapServers(kafkaProperties.getBootstrapServers()) //
+                .overrides(properties) //
+                .build();
     }
 }
