9ba5ee325f2f541a0dee4a561dcf7e923f7d8bf9
Aurélien Leboulanger <35097656+aleboulanger@users.noreply.github.com>
2018-06-25 12:56:15 +0000

    fix(TDP-4668): Config should not match if zipkin property is not set (#381)
    * force the setup of the spring.zipkin.enabled property in order to load the ZipkinKafkaSenderConfiguration
    
    * Provide a default bootstrapServer value (from KafkaProperties spring bean) in order to avoid NPE in the KafkaSender builder

--- daikon-spring/daikon-spring-zipkin/src/main/java/org/talend/daikon/zipkin/ZipkinKafkaSenderConfiguration.java
+++ daikon-spring/daikon-spring-zipkin/src/main/java/org/talend/daikon/zipkin/ZipkinKafkaSenderConfiguration.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,7 +24,7 @@ import zipkin2.reporter.kafka11.KafkaSender;
  */
 @Configuration
 @ConditionalOnClass(ByteArraySerializer.class)
-@ConditionalOnProperty(value = "spring.zipkin.enabled", havingValue = "true", matchIfMissing = true)
+@ConditionalOnProperty(value = "spring.zipkin.enabled", havingValue = "true")
 @EnableConfigurationProperties(ZipkinKafkaProperties.class)
 public class ZipkinKafkaSenderConfiguration {
 
@@ -31,16 +32,22 @@ public class ZipkinKafkaSenderConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public Sender kafkaSender(ZipkinKafkaProperties kafkaProperties) {
-        LOG.info("building zipkin kafka sender with brokers [{}]", kafkaProperties.getBootstrapServers());
+    public Sender kafkaSender(ZipkinKafkaProperties zipkinKafkaProperties, KafkaProperties kafkaProperties) {
+        String bootstrapServers = zipkinKafkaProperties.getBootstrapServers();
+
+        if (bootstrapServers == null) {
+            bootstrapServers = String.join(",", kafkaProperties.getBootstrapServers());
+        }
+
+        LOG.info("building zipkin kafka sender with brokers [{}]", bootstrapServers);
 
         Map<String, Object> properties = new HashMap<>(2);
         properties.put("key.serializer", ByteArraySerializer.class.getName());
         properties.put("value.serializer", ByteArraySerializer.class.getName());
 
         return KafkaSender.newBuilder() //
-                .topic(kafkaProperties.getTopic()) //
-                .bootstrapServers(kafkaProperties.getBootstrapServers()) //
+                .topic(zipkinKafkaProperties.getTopic()) //
+                .bootstrapServers(bootstrapServers) //
                 .overrides(properties) //
                 .build();
     }
