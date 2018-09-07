8b3c445b04539e954133744dbef3fe39ff5b9adf
Damien Prot <dprot@talend.com>
2018-09-07 10:58:16 +0200

    feat(TDQ-15761): Translate word patterns into mongo/regex patterns (#393)
    * feat(TDQ-15761): Translate word patterns into mongo/regex patterns
    
    * feat(TDQ-15761): Word pattern PR review

---
+++ daikon/src/main/java/org/talend/daikon/pattern/word/WordPattern.java
@@ -0,0 +1,90 @@
+package org.talend.daikon.pattern.word;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Define the set of all supported word patterns (both case-insensitive and case-sensitive)
+ */
+public enum WordPattern {
+    // @formatter:off
+    WORD(
+            "[Word]",
+            "[" + WordPatternToRegexConstants.CHAR + "]{2,}",
+            WordPatternToRegexConstants.UPPER_CHAR + "[" + WordPatternToRegexConstants.LOWER_CHAR + "]{1,}"),
+    LOWER_WORD(
+            "[word]",
+            "[" + WordPatternToRegexConstants.CHAR + "]{2,}",
+            "[" + WordPatternToRegexConstants.LOWER_CHAR + "]{2,}"),
+    UPPER_WORD(
+            "[WORD]",
+            "[" + WordPatternToRegexConstants.CHAR + "]{2,}",
+            "[" + WordPatternToRegexConstants.UPPER_CHAR + "]{2,}"),
+    LOWER_CHAR(
+            "[char]",
+            "[" + WordPatternToRegexConstants.CHAR + "]",
+            "[" + WordPatternToRegexConstants.LOWER_CHAR + "]"),
+    UPPER_CHAR(
+            "[Char]",
+            "[" + WordPatternToRegexConstants.CHAR + "]",
+            "[" + WordPatternToRegexConstants.UPPER_CHAR + "]"),
+    NUMBER(
+            "[number]",
+            "[" + WordPatternToRegexConstants.DIGIT + "]{2,}"),
+    DIGIT(
+            "[digit]",
+            "[" + WordPatternToRegexConstants.DIGIT + "]"),
+    ALPHANUMERIC(
+            "[alnum]",
+            "[" + WordPatternToRegexConstants.DIGIT + "|" + WordPatternToRegexConstants.CHAR + "]{2,}"),
+    IDEOGRAM(
+            "[Ideogram]",
+            "[" + WordPatternToRegexConstants.IDEOGRAM + "]"),
+    IDEOGRAM_SEQUENCE(
+            "[IdeogramSeq]",
+            "[" + WordPatternToRegexConstants.IDEOGRAM + "]{2,}"),
+    ALPHANUMERIC_CJK(
+            "[alnum(CJK)]",
+            "[" + WordPatternToRegexConstants.DIGIT + "|" + WordPatternToRegexConstants.IDEOGRAM + "]{2,}");
+    // @formatter:on
+    private static final Map<String, WordPattern> lookup = new HashMap<>();
+
+    static {
+        for (WordPattern value : WordPattern.values()) {
+            lookup.put(value.getPattern(), value);
+        }
+    }
+
+    public static WordPattern get(final String pattern) {
+        return lookup.get(pattern);
+    }
+
+    private final String pattern;
+
+    private final String caseInsensitive;
+
+    private final String caseSensitive;
+
+    WordPattern(final String pattern, final String caseInsensitive, final String caseSensitive) {
+        this.pattern = pattern;
+        this.caseInsensitive = caseInsensitive;
+        this.caseSensitive = caseSensitive;
+    }
+
+    WordPattern(final String pattern, final String caseInsensitive) {
+        this(pattern, caseInsensitive, caseInsensitive);
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public String getCaseInsensitive() {
+        return caseInsensitive;
+    }
+
+    public String getCaseSensitive() {
+        return caseSensitive;
+    }
+
+}
