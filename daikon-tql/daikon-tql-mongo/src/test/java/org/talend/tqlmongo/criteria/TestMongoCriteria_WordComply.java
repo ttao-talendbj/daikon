5bd377b5a7096ed5661fbe4e34fe0b9d1efc7969
gmzoughi <gmzoughi@talend.com>
2018-09-18 10:05:24 +0200

    feat(2681): Add tests. (#396)

--- daikon-tql/daikon-tql-mongo/src/test/java/org/talend/tqlmongo/criteria/TestMongoCriteria_WordComply.java
+++ daikon-tql/daikon-tql-mongo/src/test/java/org/talend/tqlmongo/criteria/TestMongoCriteria_WordComply.java
@@ -83,4 +83,62 @@ public class TestMongoCriteria_WordComply extends TestMongoCriteria_Abstract {
         List<Record> records = this.getRecords(criteria);
         Assert.assertEquals(0, records.size());
     }
+
+    @Test
+    public void testParseFieldCompliesPattern9() {
+        Criteria criteria = doTest("name wordComplies '[Ideogram]'");
+        Criteria expectedCriteria = Criteria.where("name").regex("^[\\p{script=Han}]$");
+        Assert.assertEquals(expectedCriteria, criteria);
+        List<Record> records = this.getRecords(criteria);
+        Assert.assertEquals(0, records.size());
+    }
+
+    @Test
+    public void testParseFieldCompliesPattern10() {
+        Criteria criteria = doTest("name wordComplies '[char]'");
+        Criteria expectedCriteria = Criteria.where("name").regex("^[\\p{Ll}]$");
+        Assert.assertEquals(expectedCriteria, criteria);
+        List<Record> records = this.getRecords(criteria);
+        Assert.assertEquals(0, records.size());
+    }
+
+    @Test
+    public void testParseFieldCompliesPattern11() {
+        Criteria criteria = doTest("name wordComplies '[Char]'");
+        Criteria expectedCriteria = Criteria.where("name").regex("^[\\p{Lu}]$");
+        Assert.assertEquals(expectedCriteria, criteria);
+        List<Record> records = this.getRecords(criteria);
+        Assert.assertEquals(0, records.size());
+    }
+
+    @Test
+    public void testParseFieldCompliesPattern12() {
+        Criteria criteria = doTest("name wordComplies '[alnum]'");
+        Criteria expectedCriteria = Criteria.where("name").regex("^[\\p{Nd}|\\p{Lu}\\p{Ll}]{2,}$");
+        Assert.assertEquals(expectedCriteria, criteria);
+        List<Record> records = this.getRecords(criteria);
+        Assert.assertEquals(3, records.size());
+        Assert.assertEquals(1, records.stream().filter(r -> r.getName().equals("Ghassen")).count());
+        Assert.assertEquals(1, records.stream().filter(r -> r.getName().equals("Benoit")).count());
+        Assert.assertEquals(1, records.stream().filter(r -> r.getName().equals("ghassen")).count());
+
+    }
+
+    @Test
+    public void testParseFieldCompliesPattern13() {
+        Criteria criteria = doTest("name wordComplies '[IdeogramSeq]'");
+        Criteria expectedCriteria = Criteria.where("name").regex("^[\\p{script=Han}]{2,}$");
+        Assert.assertEquals(expectedCriteria, criteria);
+        List<Record> records = this.getRecords(criteria);
+        Assert.assertEquals(0, records.size());
+    }
+
+    @Test
+    public void testParseFieldCompliesPattern14() {
+        Criteria criteria = doTest("name wordComplies '[alnum(CJK)]'");
+        Criteria expectedCriteria = Criteria.where("name").regex("^[\\p{Nd}|\\p{script=Han}]{2,}$");
+        Assert.assertEquals(expectedCriteria, criteria);
+        List<Record> records = this.getRecords(criteria);
+        Assert.assertEquals(0, records.size());
+    }
 }
